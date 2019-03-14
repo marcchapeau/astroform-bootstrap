73c732fc806f3d1c4119307d034df7b2a63f7243
Marc Chapeau <marcchapeau@gmail.com>
2019-03-14 15:29:52 +0100

    changing package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'chap:astroform-bootstrap',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Bootstrap templating for chap:astroform',
   documentation: 'README.md'
 })
